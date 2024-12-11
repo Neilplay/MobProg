d26e815aced665911a08738bd6b0d65bc8a8875f
InfoNuggetHub <rodillon.javy.32181@gmail.com>
2024-12-11 15:51:01 +0800

    remove the qr scanner feature

--- ewallet-app/App.tsx
+++ ewallet-app/App.tsx
@@ -3,14 +3,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AccountManager from './components/AccountManager';
 import TransactionScreen from './components/TransactionScreen';
-import QRCodeScanner from './components/QRCodeScanner';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'react-native';
 
 type RootStackParamList = {
   AccountManager: undefined;
   TransactionScreen: undefined;
-  QRCodeScanner: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -23,7 +21,6 @@ export default function App() {
         <Stack.Navigator initialRouteName="AccountManager" screenOptions={{ headerShown: false }}>
           <Stack.Screen name="AccountManager" component={AccountManager} />
           <Stack.Screen name="TransactionScreen" component={TransactionScreen} />
-          <Stack.Screen name="QRCodeScanner" component={QRCodeScanner} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
